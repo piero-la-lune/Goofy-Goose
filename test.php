0c3efea29292c9648cfecbdae524a7a040345fd0
piero-la-lune <piero-la-lune@derivoile.fr>
2014-09-03 15:19:15 +0200

    Fichier inutile.

--- test.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-define('PHPPREFIX', '<?php /* '); # Prefix to encapsulate data in php code.
-define('PHPSUFFIX', ' */ ?>'); # Suffix to encapsulate data in php code.
-
-file_put_contents('test2.json', PHPPREFIX.base64_encode(gzdeflate(serialize(json_decode(file_get_contents('test.json'), true)))).PHPSUFFIX);
-
-?>
